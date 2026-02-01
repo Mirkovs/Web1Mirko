94bda04835005ba1b6f3c82c20acfacd35334083
Mirkovs <mirkovs@example.com>
2026-02-01 14:40:46 +0100

    Update site URL and base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
-const site = "https://YOUR_GH_USER.github.io";
-const base = "/YOUR_REPO"; // si publicas en repo (no user page). Para user page, pon "".
+const site = "https://mirkosas.github.io";
+const base = "/Plantilla1Mirkosas/"; // si publicas en repo (no user page). Para user page, pon "".
 
 export default defineConfig({
   site,
